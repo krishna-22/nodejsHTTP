a5ff58ba2f80b9c578b3d0fb3fa7a2664b9cb179
krishna-22 <goginenisaikiran31677@gmail.com>
2020-04-04 10:35:58 +0530

    secondary

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const http=require('http')
+const fs = require('fs')
+const path = require('path')
 const hostname = 'localhost'
 const port = 3000
 
 const server = http.createServer((req,res)=>{
-    console.log(req.headers)
-    res.statusCode=200
-    res.setHeader('content-Type','text/html')
-    res.end('<html><body>hello world</body></html>')
+    console.log(req.url,req.method)
+
+    if(req.method=='GET')
+    {
+          if(req.url=='/') 
+          fileurl='/index.html'
+          else 
+          fileurl=req.url
+
+          var filepath = path.resolve('./public'+fileurl)
+          const fileExt= path.extname(filepath)
+          if(fileExt=='.html')
+          {
+              fs.exists(filepath,(exists)=>{
+                  if(!exists)
+                  {
+                    res.statusCode=404
+                    res.setHeader('content-Type','text/html')
+                    res.end(' error 404 not found')
+                    return
+                  }
+                  res.statusCode=200
+                  res.setHeader('content-Type','text/html')
+                  fs.createReadStream(filepath).pipe(res)
+
+              })
+          }
+          else
+        {
+            res.statusCode=404
+                    res.setHeader('content-Type','text/html')
+                    res.end(' error 404 .Its not Html page')
+        }
+          
+
+    }
+    else
+    {
+        res.statusCode=404
+                    res.setHeader('content-Type','text/html')
+                    res.end(' Method Not Suported')
+    }
+    
 })
 
 server.listen(port,hostname,()=>{
